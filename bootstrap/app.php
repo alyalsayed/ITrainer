fc66a93d9d89594d53b9f462ae4d3caccc6a8c17
alyalsayed <alyalsayed56@yahoo.com>
2024-11-05 01:13:51 +0200

    feat: add localization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,20 +11,24 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(
         function (Middleware $middleware) {
             $middleware->alias([
                 'role' => CheckRole::class,
-                'update.last.seen' => UpdateLastSeenMiddleware::class
-            
+                'update.last.seen' => UpdateLastSeenMiddleware::class,
+                'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+                'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+                'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+                'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+                'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
             ]);
         },
-      
+
     )
-   
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
