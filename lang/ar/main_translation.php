83c2a905dab664b82ff2656dfee8a57c83e7283b
alyalsayed <alyalsayed56@yahoo.com>
2024-11-16 13:54:30 +0200

    feat : add localization -> arabic translation to the app

---
+++ lang/ar/main_translation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+
+    // Sidebar translation
+   "Dashboard" => "لوحة التحكم",
+   "Chat" => "الدردشة",
+   "Sessions" => "الجلسات",
+   "Tasks" => "المهام",
+   "Instructor Dashboard" => "لوحة التحكم للمدرب",
+   "Student Dashboard" => "لوحة التحكم للطالب",
+   "Attendance" => "الحضور والغياب",
+   "Notes" => "الملاحظات",
+   "Profile" => "الملف الشخصي",
+   "Notifications" => "الاشعارات",
+   "ToDoList" => "المهام",
+   "Logout" => "تسجيل الخروج",
+   // Dashboard translation
+   "Total_Tasks" => "اجمالي المهام",
+   "Total_Notes" => "اجمالي الملاحظات",
+   "Total_Students" => "اجمالي الطلاب",
+   "Total_Sessions" => "اجمالي الجلسات",
+   "Attendance_Rate" => "نسبة الحضور",
+   "Avg_Submission_Rate" => "متوسط ​​نسبة تسليم الواجبات",
+   "Attendance_Chart" => "رسم الحضور",
+   "Name" => "الاسم",
+   "Email" => "البريد الالكتروني",
+   "Submission_Rate" => "نسبة التسليم",
+
+
+];
---
+++ lang/en/main_translation.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+//    Sidebar translation
+   "Dashboard" => "Dashboard",
+   "Chat" => "Chat",
+   "Sessions" => "Sessions",
+   "Tasks" => "Tasks",
+   "Instructor Dashboard" => "Instructor Dashboard",
+   "Student Dashboard" => "Student Dashboard",
+   "Attendance" => "Attendance",
+   "Notes" => "Notes",
+   "ToDoList" => "To Do",
+   "Notifications" => "Notifications",
+   "Profile" => "Profile",
+   "Logout" => "Logout",
+   // Dashboard translation
+   "Total_Tasks" => "Total Tasks",
+   "Total_Notes" => "Total Notes",
+   "Total_Students" => "Total Students",
+   "Total_Sessions" => "Total Sessions",
+   "Attendance_Rate" => "Attendance Rate",
+   "Avg_Submission_Rate" => "Avg Submission Rate",
+   "Attendance_Chart" => "Attendance Chart",
+   "Name" => "Name",
+   "Email" => "Email",
+   "Submission_Rate" => "Submission Rate",
+
+
+
+];
